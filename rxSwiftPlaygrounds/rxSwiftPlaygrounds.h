5b20eb2c222fcfcf5fb8bc240effeb413b2e84da
Maic Lopez Saenz <maic@lopsae.com>
2018-03-19 14:14:46 -0700

    add base project for rxSwift

---
+++ rxSwiftPlaygrounds/rxSwiftPlaygrounds.h
@@ -0,0 +1,19 @@
+//
+//  rxSwiftPlaygrounds.h
+//  rxSwiftPlaygrounds
+//
+//  Created by Maic Lopez Saenz on 3/19/18.
+//  Copyright © 2018 LopSae. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for rxSwiftPlaygrounds.
+FOUNDATION_EXPORT double rxSwiftPlaygroundsVersionNumber;
+
+//! Project version string for rxSwiftPlaygrounds.
+FOUNDATION_EXPORT const unsigned char rxSwiftPlaygroundsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <rxSwiftPlaygrounds/PublicHeader.h>
+
+
