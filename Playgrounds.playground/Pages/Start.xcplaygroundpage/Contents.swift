a20b389a6a3f71191244ac9c454daa78c02f9729
Maic Lopez Saenz <maic@lopsae.com>
2019-08-02 15:38:56 -0700

    rename Playarea to Thicket in Start
    + to try a different name

--- Playgrounds.playground/Pages/Start.xcplaygroundpage/Contents.swift
+++ Playgrounds.playground/Pages/Start.xcplaygroundpage/Contents.swift
@@ -3,13 +3,15 @@
 import RxSwift
 import RxSwiftPlaygrounds
 
+// TODO: just trying a different name
+typealias Thicket = Playarea
 
-Playarea.comment("This project uses `Playarea`, a helper class to indentate")
-Playarea.comment("console output into visually distinct chunks")
-Playarea.newLine()
+Thicket.comment("This project uses `Thicket`, a helper class to indentate")
+Thicket.comment("console output into visually distinct chunks")
+Thicket.newLine()
 
 
-Playarea.example("⭕️ Playarea example") { p in
+Thicket.example("⭕️ Thicket example") { p in
   p.comment("Playgrounds are structured through the `example` function")
   p.comment("Each `example` closure receives an `Printer` that prints")
   p.comment("comments and output with a given indentation")
@@ -18,56 +20,55 @@ Playarea.example("⭕️ Playarea example") { p in
 }
 
 
-Playarea.example("⭕️ Another example") { p in
+Thicket.example("⭕️ Another example") { p in
   p.comment("Each `example` creates its own visual chunk.")
   p.print("🖨 With separated chunks it is easier to read through several `examples`")
 }
 
 
-
-Playarea.comment("""
-  For printing outside of `example` closures `Playarea` exposes the static
+Thicket.comment("""
+  For printing outside of `example` closures `Thicket` exposes the static
   `print` and `comment` functions
 
-  These functions print using `Playarea.root` which by default has no
+  These functions print using `Thicket.root` which by default has no
   indentation
   """)
-Playarea.newLine()
+Thicket.newLine()
 
 
-Playarea.comment("""
-  Notice also how `comment` automatically prefixes `Playarea.defaultCommentPrefix`,
+Thicket.comment("""
+  Notice also how `comment` automatically prefixes `Thicket.defaultCommentPrefix`,
   which by default is "// ", to every printed line.
   """)
-Playarea.newLine()
+Thicket.newLine()
 
 
-Playarea.example("⭕️ Printing shorthands `/` and `<`") { p in
+Thicket.example("⭕️ Printing shorthands `/` and `<`") { p in
   p / "The `/` operator can be used as a shorthand for `comment`"
   p < "The `<` operator can be used as a shorthand for `print`"
 }
 
 
-Playarea.example("⭕️ Disable comments") { p in
-  p / "To reduce verbosity comments can be disabled globally with `Playarea.printsComments`"
+Thicket.example("⭕️ Disable comments") { p in
+  p / "To reduce verbosity comments can be disabled globally with `Thicket.printsComments`"
 
   p < "🖨 disabling comments, no lines with ☢️ should be visible in console"
-  Playarea.printsComments = false
+  Thicket.printsComments = false
 
   p / "☢️ `comment` will no longer print!"
-  Playarea.comment("☢️ and neither `Playarea.comment`")
+  Thicket.comment("☢️ and neither `Thicket.comment`")
   p < "🖨 `print` will always print regardless of `printsComments` configuration"
 
   p < "🖨 re-enabling comments"
-  Playarea.printsComments = true
+  Thicket.printsComments = true
   p / "Comments are printed again!"
 }
 
 
-Playarea.root / """
+Thicket.root / """
   `done👑` is called at the end of the playground just to have a visual
   confirmation that the whole file has executed
   """
 
-Playarea.done👑()
+Thicket.done👑()
 
