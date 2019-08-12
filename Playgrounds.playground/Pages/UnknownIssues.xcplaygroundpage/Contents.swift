e258d0e0c50a11f1b4ac1d606c8713eacd671a2e
Maic Lopez Saenz <maic@lopsae.com>
2019-08-12 16:02:16 -0700

    add an example of an issue seen elsewhere
    + altho the issue did not reproduce...

--- Playgrounds.playground/Pages/UnknownIssues.xcplaygroundpage/Contents.swift
+++ Playgrounds.playground/Pages/UnknownIssues.xcplaygroundpage/Contents.swift
@@ -9,6 +9,60 @@ import RxSwiftPlaygrounds
 typealias Binder = Playarea
 
 
+Binder.example("⭕️ startWith vs from") { p in
+  let subject = PublishSubject<String>()
+  var disposable = Disposables.create()
+
+  let justAndStart = subject.flatMap {
+    string -> Observable<String> in
+    switch string {
+    case "one":
+      return Observable.just("first").startWith("prefix")
+    case "two":
+      return .just("second")
+    default:
+      return .error(RxError.unknown)
+    }
+  }
+
+  disposable = justAndStart.subscribe(
+    onNext: { p < "justAndStart: \($0)" })
+  subject.onNext("one")
+  subject.onNext("two")
+  disposable.dispose()
+
+
+  let fromAndJust = subject.flatMap {
+    string -> Observable<String> in
+    switch string {
+    case "one":
+      return Observable.from(["prefix", "first"])
+    case "two":
+      return .just("second")
+    default:
+      return .error(RxError.unknown)
+    }
+  }
+
+  disposable = fromAndJust.subscribe(
+    onNext: { p < "fromAndJust: \($0)" })
+  subject.onNext("one")
+  subject.onNext("two")
+  disposable.dispose()
+
+}
+
+
+//flatmap
+//  + just(first).startWith(prefix)
+//  + just(second)
+//  -> prefix first second
+//
+//flatmap
+//  + from(prefix, first)
+//  + just(second)
+//  -> prefix second first
+
 
 Binder.done👑(needsIndefiniteExecution: true)
 
