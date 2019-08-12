4968abde332eb3bf26e909ea94473365c56543bd
Maic Lopez Saenz <maic@lopsae.com>
2019-08-12 15:33:58 -0700

    add disposables and unknownIssues playgrounds

---
+++ Playgrounds.playground/Pages/UnknownIssues.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+
+
+import PlaygroundSupport
+import RxSwift
+import RxSwiftPlaygrounds
+
+
+// TODO: just trying a different name
+typealias Binder = Playarea
+
+
+
+Binder.done👑(needsIndefiniteExecution: true)
+
